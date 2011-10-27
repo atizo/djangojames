d4dace283b1c7c52f1efa86a4962ee303db13783
Marcel Eyer <maersu@judges.ch>
2011-10-27 13:23:48 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='djangojames',
-    version='0.1',
+    version='0.2',
     description="Helpers and tools for django",
     author='Marcel Eyer',
     author_email='me@maersu.ch',
