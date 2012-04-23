a5631d859e979d1710569728ed4dbcf98a552f1f
Daniel Egger <daniel@danielex.local>
2012-04-23 17:53:03 +0200

    configured default date output to iso-date

--- djangojames/forms/widgets.py
+++ djangojames/forms/widgets.py
@@ -2,3 +2,7 @@ from django.forms.widgets import DateInput
 
 class Html5DateInput(DateInput):
     input_type = 'date'
+
+    def __init__(self):
+        super(Html5DateInput, self).__init__(format="%Y-%m-%d")
+
