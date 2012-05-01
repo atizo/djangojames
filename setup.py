72f0334ce8349f057bfba5396a8fae676e9026c4
maersu <me@maersu.ch>
2012-05-01 17:04:49 +0200

    setup: use data files

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+packages = []
+data_files = []
+root_dir = 'djangojames'
+
+def fullsplit(path, result=None):
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
     name='djangojames',
@@ -7,7 +32,8 @@ setup(
     author='Marcel Eyer',
     author_email='me@maersu.ch',
     url='https://github.com/atizo/djangojames',
-    packages=find_packages(),
+    packages=packages,
+    data_files=data_files,
     zip_safe=False,
     install_requires=[
         'python-googleanalytics',
