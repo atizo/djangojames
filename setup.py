4b0f5e52017d8c79784456df90a946fd111f7338
maersu <me@maersu.ch>
2012-05-01 17:44:20 +0200

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 import os
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
 
 packages = []
 data_files = []
@@ -29,12 +27,6 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
         packages.append('.'.join(fullsplit(dirpath)))
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Tell distutils not to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
 
 setup(
     name='djangojames',
